e9b430582d9669476b0210c5f6cb18004703d0e1
Ethan Ying Wang <Yingqiu.Wang@mssa.newhorizons.com>
2024-11-18 16:59:25 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace Assignments_3._1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string evenNumbers = GetEvenNumbers();
+            Console.WriteLine(evenNumbers);
+        }
+
+        static string GetEvenNumbers()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 2; i < 100; i += 2) 
+            {
+                sb.Append(i);
+                if (i < 98) 
+                {
+                    sb.Append(", ");
+                }
+            }
+
+            return sb.ToString(); 
+        }
+    }
+}
